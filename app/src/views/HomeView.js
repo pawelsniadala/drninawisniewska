d6ee9d1732975ce172f527d1bd597c6b8f1a1163
Paweł Śniadała <biner.home@gmail.com>
2022-08-10 22:56:34 +0200

    Adding a Home view

---
+++ app/src/views/HomeView.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+// import PosterSection from "../sections/PosterSections";
+// import CastSection from "../sections/CastSection";
+// import CreatorSection from "../sections/CreatorSection";
+// import SummarySection from "../sections/SummarySection";
+// import PhotoSection from "../sections/PhotoSection";
+
+const HomeView = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, []);
+
+    return (
+        <div className="home-view">
+            <p>home-view</p>
+            {/* <PosterSection />
+            <CastSection />
+            <CreatorSection />
+            <SummarySection />
+            <PhotoSection /> */}
+        </div>
+    );
+};
+
+export default HomeView;
