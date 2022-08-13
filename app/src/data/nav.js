c165ef4a953a0f320b5601de60060354cbd2b7a9
Paweł Śniadała <biner.home@gmail.com>
2022-08-13 14:47:13 +0200

    Updating images

---
+++ app/src/data/nav.js
@@ -0,0 +1,38 @@
+// import castMainThumbnail1 from "../assets/cast/main/castMainThumbnail1.jpg"
+
+export const nav = {
+    navLogo: [
+        {
+            id: "a07d89ac-b7ef-49c3-aa3e-020c73809e7f",
+            title: "Logo",
+            path: "/"
+        }
+    ],
+    navItem: [
+        {
+            id: "09a7d739-42f1-4147-a2ed-79bc2ac7b2d6",
+            title: "O nas",
+            path: "/about-us"
+        },
+        {
+            id: "0417e32b-d424-460f-af97-dadc8efa5bde",
+            title: "Usługi",
+            path: "/services"
+        },
+        {
+            id: "982bb88c-d367-4fa2-a06a-57a71c32cd03",
+            title: "Cennik",
+            path: "/price-list"
+        },
+        {
+            id: "e2e74079-e6e6-4016-9b4f-fd33fd2f544c",
+            title: "Aktualności",
+            path: "/news"
+        },
+        {
+            id: "2a385c29-263e-49b8-a18a-c4f7f025c808",
+            title: "Kontakt",
+            path: "/contact"
+        },
+    ]
+};
---
+++ app/src/data/services.js
@@ -0,0 +1,45 @@
+import servicesDermatology from "../assets/services/servicesDermatology.jpg";
+import servicesAestheticMedicine from "../assets/services/servicesAestheticMedicine.jpg";
+import servicesHematology from "../assets/services/servicesHematology.jpg";
+import servicesCosmetology from "../assets/services/servicesCosmetology.jpg";
+import servicesPhysioterapy from "../assets/services/servicesPhysioterapy.jpg";
+import servicesCosmeticSurgery from "../assets/services/servicesCosmeticSurgery.jpg";
+
+export const services = [
+    {
+      media: servicesDermatology,
+      title: 'Dermatologia',
+      description: 'Joshua Karamaki',
+      link: `<a href='https://pl.freepik.com/zdjecia/dermatolog'>Dermatolog zdjęcie utworzone przez wavebreakmedia_micro - pl.freepik.com</a>`
+    },
+    {
+      media: servicesAestheticMedicine,
+      title: 'Medycyna estetyczna',
+      description: 'Jhon Smith',
+      link: `<a href='https://pl.freepik.com/zdjecia/mezoterapia'>Mezoterapia zdjęcie utworzone przez nensuria - pl.freepik.com</a>`
+    },
+    {
+      media: servicesHematology,
+      title: 'Hematologia',
+      description: 'Nicol Adams',
+      link: `<a href='https://pl.freepik.com/zdjecia/badania-krwi'>Badania krwi zdjęcie utworzone przez stefamerpik - pl.freepik.com</a>`
+    },
+    {
+      media: servicesCosmetology,
+      title: 'Kosmotologia',
+      description: 'Joshua Karamaki',
+      link: `<a href='https://pl.freepik.com/zdjecia/mezoterapia'>Mezoterapia zdjęcie utworzone przez lookstudio - pl.freepik.com</a>`
+    },
+    {
+      media: servicesPhysioterapy,
+      title: 'Fizjoterapia',
+      description: 'Joshua Karamaki',
+      link: `<a href='https://pl.freepik.com/zdjecia/fizjoterapia'>Fizjoterapia zdjęcie utworzone przez javi_indy - pl.freepik.com</a>`
+    },
+    {
+      media: servicesCosmeticSurgery,
+      title: 'Chirurgia plastyczna',
+      description: 'Jhon Smith',
+      link: `<a href='https://pl.freepik.com/zdjecia/kobieta'>Kobieta zdjęcie utworzone przez wayhomestudio - pl.freepik.com</a>`
+    }
+];
