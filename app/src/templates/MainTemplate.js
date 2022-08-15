12fe29b8ff2a8827766bbeffb9d7f4b66f65159c
Paweł Śniadała <biner.home@gmail.com>
2022-08-15 22:33:38 +0200

    Adding packages to the project

--- app/src/templates/MainTemplate.js
+++ app/src/templates/MainTemplate.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import PropTypes from "prop-types";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
@@ -5,19 +6,30 @@ import "bootstrap";
 // import "photoswipe/dist/default-skin/default-skin.css";
 import "../styles/app.scss";
 // import "@fortawesome/fontawesome-free/css/all.min.css";
-
-// import 'react-lazy-load-image-component/src/effects/blur.css';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-// import 'react-image-lightbox/style.css';
-// import 'aos/dist/aos.css';
+import 'react-image-lightbox/style.css';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 
+const MainTemplate = ({ children }) => {
+    useEffect(() => {
+        AOS.init({
+            once: true,
+            delay: 0,
+            duration: 800,
+            offset: 0,
+            easing: 'ease-in-out',
+        });
+    }, []);
 
-const MainTemplate = ({ children }) => (
-    <div className="app">
-        {children}
-    </div>
-);
+    return (
+        <div className="app">
+            {children}
+        </div>
+    );
+};
 
 MainTemplate.propTypes = {
     children: PropTypes.element.isRequired
