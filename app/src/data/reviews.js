e1e8752884aacad0cc2189752cb6f872ac4af9c1
Paweł Śniadała <biner.home@gmail.com>
2022-08-15 22:40:10 +0200

    Adding reviews data

---
+++ app/src/data/reviews.js
@@ -0,0 +1,43 @@
+// import team1 from "../assets/team/team1.jpg";
+// import team2 from "../assets/team/team2.jpg";
+// import team3 from "../assets/team/team3.jpg";
+// import team4 from "../assets/team/team4.jpg";
+
+export const reviews = [
+    {
+        feedback: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas placerat magna ut tellus consectetur, quis pellentesque arcu mattis. Aliquam tempor purus et vehicula dapibus. Quisque hendrerit, massa vitae tincidunt mattis, urna purus euismod nisi, at molestie lectus.',
+        name: 'Clara Bertoletti',
+        title: 'MUI lover',
+        avatar: 'https://assets.maccarianagency.com/avatars/img1.jpg',
+    },
+    {
+        feedback: 'Curabitur semper dictum odio quis tincidunt. Etiam accumsan pretium quam eget blandit. Aliquam ligula erat, tempus et metus et, varius lobortis massa. Morbi congue posuere nulla. Ut nibh mauris, luctus non scelerisque ac, molestie vitae nisl.',
+        name: 'Jhon Anderson',
+        title: 'Senior Frontend Developer',
+        avatar: 'https://assets.maccarianagency.com/avatars/img2.jpg',
+    },
+    {
+        feedback: 'Donec a nisl nec felis molestie aliquet eget eget mauris. Morbi sed lorem id ante efficitur tincidunt. Aliquam erat volutpat. Integer hendrerit scelerisque nunc, ut volutpat felis ultrices eu. Aenean eu risus pulvinar, tempus velit eget, fringilla nisl.',
+        name: 'Chary Smith',
+        title: 'SEO at Comoti',
+        avatar: 'https://assets.maccarianagency.com/avatars/img3.jpg',
+    },
+    {
+        feedback: 'Nunc eget ultricies mi. Nunc dignissim, orci at varius vulputate, mauris enim mollis ipsum, ut fringilla massa tellus at magna. Nullam tincidunt arcu ac cursus dapibus. Duis consectetur quam vitae magna faucibus aliquam. Nam tempus interdum facilisis.',
+        name: 'Clara Bertoletti',
+        title: 'MUI lover',
+        avatar: 'https://assets.maccarianagency.com/avatars/img1.jpg',
+    },
+    {
+        feedback: 'Vestibulum eget ex sit amet sapien interdum scelerisque. Nulla ornare, est ac scelerisque vehicula, lectus erat tempus metus, vel vulputate dolor felis a libero. Ut vitae neque ac magna dictum congue.',
+        name: 'Jhon Anderson',
+        title: 'Senior Frontend Developer',
+        avatar: 'https://assets.maccarianagency.com/avatars/img2.jpg',
+    },
+    {
+        feedback: 'Nam ut commodo lectus. Etiam pretium ullamcorper lectus sagittis pulvinar. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel egestas nunc. Aliquam et lobortis ipsum. Nam consectetur molestie suscipit. Pellentesque dapibus pretium laoreet.',
+        name: 'Chary Smith',
+        title: 'SEO at Comoti',
+        avatar: 'https://assets.maccarianagency.com/avatars/img3.jpg',
+    },
+];
